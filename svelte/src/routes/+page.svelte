9b5653485f678e46ac3d1af306cce39d2cac3062
Georg <georg.windhaber@gmail.com>
2024-12-05 17:51:28 +0100

    Add input styling

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -20,7 +20,7 @@
 	};
 </script>
 
-<main>
+<main class="p-5">
 	<ul>
 		{#each todos as todo}
 			<li>
@@ -30,7 +30,8 @@
 		{/each}
 	</ul>
 
-	<form onsubmit={newTodo}>
-		<input bind:value={newTodoText} />
+	<form onsubmit={newTodo} class="flex gap-5 mt-5">
+		<input bind:value={newTodoText} class="bg-slate-200 focus:bg-slate-300 p-3 rounded-full" />
+		<button class="size-12 bg-slate-300 active:bg-slate-400 rounded-full">+</button>
 	</form>
 </main>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -20,7 +20,7 @@ const addTodo = () => {
 </script>
 
 <template>
-  <main>
+  <main class="p-5">
     <ul>
       <li v-for="todo in todos" :key="todo.id">
         <input type="checkbox" v-model="todo.done" />
@@ -28,8 +28,9 @@ const addTodo = () => {
       </li>
     </ul>
 
-    <form @submit.prevent="addTodo">
-      <input v-model="newTodoText" />
+    <form @submit.prevent="addTodo" class="flex gap-5 mt-5">
+      <input v-model="newTodoText" class="bg-slate-200 focus:bg-slate-300 p-3 rounded-full" />
+      <button class="size-12 bg-slate-300 active:bg-slate-400 rounded-full">+</button>
     </form>
   </main>
 </template>
